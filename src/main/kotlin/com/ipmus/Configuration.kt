ad5c9c5fa2f48a45b6a8071a6cb8c021283da850
Mustafa Ozturk <mdozturk@yahoo.com>
2018-01-01 08:14:13 -0500

    Removed app engine

--- src/main/kotlin/com/ipmus/Configuration.kt
+++ src/main/kotlin/com/ipmus/Configuration.kt
@@ -9,6 +9,6 @@ import org.slf4j.LoggerFactory
 object Configuration {
     //private val input = javaClass.classLoader.getResourceAsStream("config.txt")
     //private val lines = if (input != null) input.bufferedReader().lines().toArray() else arrayOf()
-    private val defaultLoc = "~/mtlstore"
+    private val defaultLoc = "mtlstore"
     val dataLocation = if (System.getProperty("os.name").toLowerCase().contains("windows")) "c:/temp/mtlStore" else defaultLoc
 }
